b9d78b3137bf85cbbf3cafe04a1b87303de03c7f
HarrisonIbarra <116966095+HarrisonIbarra@users.noreply.github.com>
2023-10-06 18:43:01 -0500

    Add files via upload

---
+++ Dashboard-Tienda/Contrels/procesarDatosRegistro.php
@@ -0,0 +1,53 @@
+<?php
+// Conexión a la base de datos
+$servidorDb = "localhost";
+$usuarioDb = "root";
+$contrasenaDb = "1023974256";
+$nombreDb = "gpamotors";
+
+$conn = new mysqli($servidorDb, $usuarioDb, $contrasenaDb, $nombreDb);
+
+// Verifica la conexión
+if ($conn->connect_error) {
+    die("Error de conexión a la base de datos: " . $conn->connect_error);
+}
+        // Captura los datos del formulario
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nombres = $_POST["nombres"];
+        $apellidos = $_POST["apellidos"];
+        $tipoDocumento = $_POST["tipoDocumento"];
+        $documento = $_POST["documento"];
+        $telefono = $_POST["telefono"];
+        $direccion = $_POST["direccion"];
+        $genero = $_POST["genero"];
+        $correo = $_POST["correo"];
+        $contrasena = $_POST["password"];
+        $confirmarContrasena = $_POST["confirmarContrasena"];
+
+        if ($contrasena !== $confirmarContrasena) {
+            echo "Las contraseñas no coinciden. Por favor, inténtalo de nuevo.";
+            return false;
+        }
+        
+        // Encriptar la contraseña utilizando password_hash
+        $hashed_password = password_hash($contrasena, PASSWORD_DEFAULT);
+
+        // Prepara una consulta SQL para insertar los datos en la tabla
+        $sql = "INSERT INTO usuarios (usuNombre, usuApellido, usuTipoDocumento, usuDocumento, usuTelefono, usuDireccion, usuGenero, usuCorreo, usuContrasena) VALUES ('$nombres', '$apellidos', '$tipoDocumento', '$documento', '$telefono', '$direccion', '$genero', '$correo', '$hashed_password')";
+        
+
+        // Ejecuta la consulta
+        if ($conn->query($sql) === TRUE) {
+            echo "Datos almacenados correctamente";
+            header("Location: ../login.html");
+            exit();
+        } else {
+            echo "Error al almacenar datos: " . $conn->error;
+        }
+    
+        // Cierra la conexión a la base de datos
+        $conn->close();
+    }
+    
+
+?>
